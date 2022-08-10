fa40205218c1c54624e49f2580b041a9389a31aa
Joseph Cauchi <jcauchi@novibet.com>
2022-08-10 11:01:38 +0200

    Add project files.

---
+++ WcfPoc/Controllers/AgentServiceController.cs
@@ -0,0 +1,33 @@
+﻿using AgentData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WcfPoc.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AgentServiceController : ControllerBase
+	{
+
+		private readonly ILogger<AgentServiceController> _logger;
+
+		public AgentServiceController(ILogger<AgentServiceController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public async Task<bool> Get()
+		{
+			AgentServiceClient agentServiceClient = new AgentServiceClient();
+			GetAllRequest getAllRequest = new GetAllRequest();
+			var result = await agentServiceClient.GetAllAsync(getAllRequest);
+			return true;
+		}
+
+		/*
+		 For more INFO follow: https://www.thecodebuzz.com/consuming-wcf-web-services-in-net-core-best-practices/
+		 */
+	}
+}
